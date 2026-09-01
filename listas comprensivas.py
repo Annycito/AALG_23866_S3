9c5d44d693089651b9392e7649bfbaeb7f508d93
Anny <annyperezsc2212@gmail.com>
2026-08-31 20:52:18 -0500

    Ejercicio multiplos 3

--- listas comprensivas.py
+++ listas comprensivas.py
@@ -1,6 +1,6 @@
 #{2*x | x e N, x <= 10}     // CONJUNTO DE LOS PRIMEROS 10 NÚMEROS PARES
-a = [2*x for x in range(1,11,1)]    
-print(a)
+#a = [2*x for x in range(1,11,1)]    
+#print(a)
 
 #Ejercicio 2 lista intencional para devolverme los numero multiplos de 3 a partir del 4 hasta el 20
 b = [x for x in range(4,21) if x % 3 == 0]
